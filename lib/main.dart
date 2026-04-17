126b6b5ece883c5e00474e02db11553a6e83535e
Eden <Jtmcflea@gmail.com>
2026-04-16 19:14:11 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,75 @@ import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:saver_gallery/saver_gallery.dart';
 
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// main root entry point for the app
 void main() {
-  runApp(const MyApp());
+  runApp(
+    // routes are individual pages within the app, which must be listed in here
+    MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+        '/image_to_image': (context) => const MyApp(),
+      },
+    ),
+  ); //MaterialApp
+}
+
+// Homepage
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome to the PystenCup!'),
+        backgroundColor: Colors.cyan,
+        foregroundColor: Colors.white,
+      ), // AppBar
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/image_to_image');
+              },
+              child: const Text("Image > Image Encoding"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/image_to_image');
+              },
+              child: const Text("Image > Image Decoding"),
+            ),
+            SizedBox(height: 25.0),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Audio > Image Encoding"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Audio > Image Decoding"),
+            ),
+            SizedBox(height: 25.0),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Text > Image Encoding"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Text > Image Decoding"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -37,10 +104,27 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   // image picker vars
   File? _image;
+  File? _secretImage;
+  File? _publicImage;
   final _picker = ImagePicker();
 
-  // android default download dir
-  final _downloadDir = "/storage/emulated/0/Download";
+  // function to select secret image
+  void _pickSecret() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      _secretImage = File(pickedFile.path);
+      setState(() {});
+    }
+  }
+
+  // function to select public image
+  void _pickPublic() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      _publicImage = File(pickedFile.path);
+      setState(() {});
+    }
+  }
 
   // helper function for picking an image
   void _pickImage() async {
@@ -51,12 +135,31 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
+  void _genImage(BuildContext context) async {
+    // image objects, just using them to check that the resolution is the same
+    final pubObject = await decodeImageFromList(
+      _publicImage!.readAsBytesSync(),
+    );
+    final secObject = await decodeImageFromList(
+      _secretImage!.readAsBytesSync(),
+    );
+    // check that images are the same resolution
+    if (pubObject.width != secObject.width ||
+        pubObject.height != secObject.height) {
+      _showToast(context, "Error: Images must be the same resolution");
+    } else {
+      // run the python script
+      // await SeriousPython.run();
+    }
+  }
+
   // runs the python script for creating an image preview
   void _genPreview() async {
     // make sure the image is there
     if (_image == null) {
       throw (Stream.error("No image to make look like crap!"));
     }
+
     // directory where the image is / where the output is saved
     final imageDir = _image!.parent;
 
@@ -76,11 +179,11 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {});
   }
 
-  void _showToast(BuildContext context) {
+  void _showToast(BuildContext context, String text) {
     final scaffold = ScaffoldMessenger.of(context);
     scaffold.showSnackBar(
       SnackBar(
-        content: const Text("File saved to gallery"),
+        content: Text(text),
         action: SnackBarAction(
           label: 'Close',
           onPressed: scaffold.hideCurrentSnackBar,
@@ -110,26 +213,26 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: .center,
           children: [
             ElevatedButton(
-              onPressed: _genPreview,
-              child: Text("Generate Preview"),
+              onPressed: _pickSecret,
+              child: _secretImage == null
+                  ? Text("Upload Secret Image")
+                  : Text("Secret Image Selected!"),
             ),
-            _image == null
-                ? Text("Please upload an image")
-                : Image.file(_image!),
-            if (_image != null)
+            ElevatedButton(
+              onPressed: _pickPublic,
+              child: _publicImage == null
+                  ? Text("Upload Public Image")
+                  : Text("Public Image Selected!"),
+            ),
+            if (_secretImage != null && _publicImage != null)
               ElevatedButton(
-                onPressed: _downloadFile,
-                child: (Text("Download Output")),
+                onPressed: () {
+                  _genImage(context);
+                },
+                child: Text("Generate Image"),
               ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _pickImage();
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.camera_alt),
       ),
     );
   }
